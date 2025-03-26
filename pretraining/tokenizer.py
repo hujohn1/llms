92c9e5eb5c9f078b514293b1aec02a16ca43870f
John Hu <johnhu1235@gmail.com>
2025-03-26 00:04:03 -0400

    created working tokenizer

--- pretraining/tokenizer.py
+++ pretraining/tokenizer.py
@@ -1,18 +1,48 @@
 import re
 import urllib.request
+from collections import defaultdict
 
 URL=("https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt")
 filepath='the-verdict.txt'
 urllib.request.urlretrieve(URL, filepath)
 
-try:
-    with open('the-verdict.txt') as file:
-        rawtext = file.read()
-        print(rawtext[:99])
-        result=re.split(r'([:;,.?!"()]|\s)', rawtext[:99])
-        rstripped=map(lambda x: x.strip(), result)
-        final=[x for x in rstripped if x!='']
-        print(final)
+class Encoder():
+    def __init__(self, href):
+        self.href= href
+        self.rawtext=""
+        self.mapping=[]         #mapping from unique tokens->id
+        self.inv_mapping = []   #mapping from id->unique tokens
 
-except:
-    print("error")
+    def __str__(self):
+        if not self.rawtext:
+            with open('the-verdict.txt') as file:
+                self.rawtext=file.read()
+                rt=re.split(r'([:;,.?!"()]|--|\s)', self.rawtext)
+                rtt = final=[x for x in rt if x!='']
+                result = []
+                for char in rtt:
+                    if char in self.mapping:
+                        result.append({char: self.mapping[char]})
+                    else:
+                        result.append({char: None})
+                return f"Token to ID mapping: {result}"
+            
+    def encode(self):
+        if not self.mapping:
+            with open(self.href) as file:
+                rawtext = file.read()
+                result=re.split(r'([:;,.?!"()]|--|\s)', rawtext)
+                rstripped=map(lambda x: x.strip(), result)
+                final=[x for x in rstripped if x!='']
+                dictionary={word: idx for idx, word in enumerate(set(final))}
+                self.mapping = dictionary
+                
+
+    def decode(self):
+        if not self.inv_mapping:
+            self.inv_mapping = {self.mapping[key]: key for key in self.mapping}
+
+e = Encoder(filepath)
+e.encode()
+e.decode()
+print(str(e))
